3c00566d31b4c2cee3c4c841248542823db74e5f
mudstartommy <mudstargaming@gmail.com>
2022-11-12 12:23:05 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,3 +6,11 @@ client = discord.Client()
 async def on_ready():
     print("Online")
     await client.change_presence(game=discord.Game(name="'Hidden'"))
+
+@client.event
+async def on_message(message):
+    if message.author == client.user:
+        return
+    if message.content == "Hello":
+        await client.send_message(message.channel, "World")
+client.run(TOKEN)
